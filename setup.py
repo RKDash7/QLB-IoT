c2000be4543d0e34bb8daac1a1fdfec4c3f97404
RKDash7 <rkdash@outr.ac.in>
2025-03-02 18:47:34 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, QLB-IoT
+
+setup(
+    name="QLB-IoT",
+    version="0.1",
+    author="Ranjan Kumar Dash",
+    author_email="rkdash@outr.ac.in",
+    description="QLB-IoT: Intelligent and Trust-Aware Routing for IoT-Edge Networks with Blockchain-Assisted Q-Learning",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+)
